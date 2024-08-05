0ebcee8d93d000baed16e5bbfc8c3992372052d7
Revanth Srinivas Bolla <113007630+Revanth-03@users.noreply.github.com>
2024-08-05 16:50:55 +0530

    Initial commit

---
+++ src/index.mjs
@@ -0,0 +1,14 @@
+// const str = "rectangle";
+// let resultstr = "";
+// for (let elememt of str) {
+//   resultstr = elememt + resultstr;
+// }
+// // console.log(resultstr);
+
+// //destructing
+
+// const obj = { a: "hello", name: "Revanth" };
+
+// let { a } = obj;
+
+// // console.log(a);
